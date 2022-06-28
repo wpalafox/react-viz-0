c7b704bc31efa9ddc343822f8b5704e004bb36f6
wpalafox <wpalafox@gmail.com>
2022-06-28 17:55:43 -0600

    update

---
+++ src/components/PieChart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { VictoryPie } from "victory";
+
+
+
+
+
+
+export default function PieChart() {
+  return <VictoryPie />;
+
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import './index.css';
 import App from './App';
+import Navbar from './components/Navbar.js';
+import Footer from './components/Footer.js';
 import reportWebVitals from './reportWebVitals';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<App />} />
+     
+      
+
+      
+    
+    
+    </Routes>
+    <Footer />
+  
+  
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
