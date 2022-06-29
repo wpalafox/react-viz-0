8b9dbb6c0029f7af7095c10a41d9e0c932aba733
wpalafox <wpalafox@gmail.com>
2022-06-28 19:12:41 -0600

    block 1 done today

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Imperva from '../images/imperva_footer.jpeg';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+            <div className="card bg-dark text-white">
+                <img className="card-img" src={Imperva} alt="Card image" />
+                
+            </div>
+
+
+
+    </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Vulnerability Analytics Dashboard</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                
+               
+                <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dashboard Type (Dropdown)
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="dropdown-item" href="#">Manager</a>
+                        <a className="dropdown-item" href="#">Security Champion</a>
+                        <a className="dropdown-item" href="#">Technical Product</a>
+                        <a className="dropdown-item" href="#">Business Product</a>
+                    </div>
+                </li>
+
+
+
+                <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Aggregration Type (Dropdown)
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="dropdown-item" href="#">Action</a>
+                        <a className="dropdown-item" href="#">Another action</a>
+                        <a className="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+              
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Generate</button>
+            </form>
+        </div>
+        </nav>
+
+
+
+      </div>
+    )
+  }
 }
+
